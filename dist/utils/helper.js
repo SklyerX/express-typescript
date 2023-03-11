21709006e2d0c772c41de7d49f2559c0d331dd39
Skyler.Ab <85718273+SklyerX@users.noreply.github.com>
2023-03-11 18:14:26 -0500

    clean up

--- dist/utils/helper.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sleep = void 0;
-const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
-exports.sleep = sleep;
